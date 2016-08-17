dfd9130e138816cbafad3ba87fd091f495ecda1b
andela-tadesanya <adeyemi.adesanya@andela.com>
2016-08-17 17:08:21 +0100

    setup admin site

--- djangobook/book/models.py
+++ djangobook/book/models.py
@@ -7,9 +7,22 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50,
                             unique=True)
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "categories"
 
 
 class Book(models.Model):
     name = models.CharField(max_length=255)
     ISBN = models.CharField(max_length=25)
     category = models.ForeignKey(Category)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "books"
