f51f1a89f67b070cd53bfe0395bfaa88886931f8
andela-tadesanya <adeyemi.adesanya@andela.com>
2016-08-17 13:33:59 +0100

    create book and category models

--- djangobook/book/models.py
+++ djangobook/book/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50,
+                            unique=True)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=25)
+    category = models.ForeignKey(Category)
